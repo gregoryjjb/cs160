5fdefbe2d802fedc201450e159cb0cb83b54bfc8
Dean Johnson <dean@deanljohnson.com>
2018-02-23 12:38:02 -0800

    Added barebones processing project with NetBeans gitignore setup

---
+++ CVProcessor/main.cpp
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: dean
+ *
+ * Created on February 23, 2018, 12:22 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
