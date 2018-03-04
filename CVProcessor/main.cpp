f73421832358ce8264b1e34c6556651f1343a962
Dean Johnson <dean@deanljohnson.com>
2018-03-04 00:16:23 -0800

    Parallelized opening and closing of images

--- CVProcessor/main.cpp
+++ CVProcessor/main.cpp
@@ -16,12 +16,49 @@
 #include <string>
 #include <vector>
 #include <thread>
+#include <future>
+
+#include <tbb/tbb.h>
 
 #include "VideoMetadata.h"
 #include "FFMPEGProcessing.h"
 #include "OpenFaceProcessing.h"
 #include "EyeLikeProcessing.h"
 #include "Utilities.h"
+
+class OpenImages
+{
+public:
+    std::vector<cv::Mat>* images;
+    const std::string* framesFormat;
+    void operator()(const tbb::blocked_range<size_t>& r) const
+    {
+        for (size_t i = r.begin(); i != r.end(); i++)
+        {
+            char buffer[128];
+            snprintf(buffer, 128, framesFormat->c_str(), i);
+            std::string input(buffer);
+            (*images)[i-1] = OpenFaceProcessing::openImage(input);
+        }
+    }
+};
+
+class SaveImages
+{
+public:
+    std::vector<cv::Mat>* images;
+    const std::string* framesFormat;
+    void operator()(const tbb::blocked_range<size_t>& r) const
+    {
+        for (size_t i = r.begin(); i != r.end(); i++)
+        {
+            char buffer[128];
+            snprintf(buffer, 128, framesFormat->c_str(), i);
+            std::string output(buffer);
+            OpenFaceProcessing::saveImage(output, (*images)[i-1]);
+        }
+    }
+};
 
 void processFrame(const cv::Mat& input, 
                   cv::Mat& output, 
@@ -31,17 +68,19 @@ void processFrame(const cv::Mat& input,
     cv::Mat_<uchar> grayImage;
     Utilities::ConvertToGrayscale_8bit(input, grayImage);
     
+    auto pupilsFuture = std::async(std::launch::async, [&]()
+    {
+        return EyeLikeProcessing::detectPupils(grayImage);
+    });
+    
     OpenFaceProcessing::FaceDataPointsRecord dataPoints
                 = OpenFaceProcessing::extractFaceDataPoints(grayImage, metadata, model);
-    
-    OpenFaceProcessing::applyFaceDataPointsToImage(input, output, dataPoints, metadata);
-    
     std::vector<cv::Vec6f> triangles = 
         OpenFaceProcessing::getDelaunayTriangles(dataPoints, metadata);
     
+    OpenFaceProcessing::applyFaceDataPointsToImage(input, output, dataPoints, metadata);
     OpenFaceProcessing::applyDelaunayTrianlgesToImage(output, triangles, metadata);
-    
-    EyeLikeProcessing::applyEyeCentersToImage(output, EyeLikeProcessing::detectPupils(grayImage));
+    EyeLikeProcessing::applyEyeCentersToImage(output, pupilsFuture.get());
 }
 
 void processSetOfFrames(std::vector<cv::Mat>::const_iterator inputIt,
@@ -88,14 +127,10 @@ void processVideo(const std::string& framesFormat,
     std::vector<cv::Mat> images((size_t)imageCount);
     std::vector<cv::Mat> processedImages((size_t)imageCount);
     
-    // Open all images of the video
-    for (int i = 1; i < imageCount + 1; i++)
-    {
-        char buffer[128];
-        snprintf(buffer, 128, framesFormat.c_str(), i);
-        std::string input(buffer);
-        images[i-1] = OpenFaceProcessing::openImage(input);
-    }
+    OpenImages openImagesLoop;
+    openImagesLoop.framesFormat = &framesFormat;
+    openImagesLoop.images  = &images;
+    tbb::parallel_for(tbb::blocked_range<size_t>(1, imageCount + 1), openImagesLoop);
     
     Utilities::uint64 tStart = Utilities::GetTimeMs64();
     
@@ -107,16 +142,12 @@ void processVideo(const std::string& framesFormat,
 
     Utilities::uint64 tEnd = Utilities::GetTimeMs64();
     
-    std::cout << "Processing Took: " << (tEnd - tStart) << "ms" << std::endl;
+    std::cout << "Frame Processing(w/o IO) Took: " << (tEnd - tStart) << "ms" << std::endl;
 
-    // Save all processed images
-    for (int i = 1; i < imageCount + 1; i++)
-    {
-        char buffer[128];
-        snprintf(buffer, 128, processedFormat.c_str(), i);
-        std::string output(buffer);
-        OpenFaceProcessing::saveImage(output, processedImages[i-1]);
-    }
+    SaveImages saveImagesLoop;
+    saveImagesLoop.framesFormat = &processedFormat;
+    saveImagesLoop.images = &processedImages;
+    tbb::parallel_for(tbb::blocked_range<size_t>(1, imageCount + 1), saveImagesLoop);
 }
 
 int main(int argc, char** argv)
@@ -125,6 +156,8 @@ int main(int argc, char** argv)
     {
         std::string inputFile = argv[1];
 
+        Utilities::uint64 tStart = Utilities::GetTimeMs64();
+        
         VideoMetadata metadata =
             FFMPEGProcessing::extractMetadata(inputFile);
         
@@ -139,6 +172,10 @@ int main(int argc, char** argv)
         processVideo("frames/out%d.png", "processed/out%d.png", metadata);
 
         FFMPEGProcessing::combineFrames("processed/out%d.png", "processed.mp4", metadata);
+        
+        Utilities::uint64 tEnd = Utilities::GetTimeMs64();
+        
+        std::cout << "Full Processing Took: " << (tEnd - tStart) << "ms" << std::endl;
     }
 
     return 0;
