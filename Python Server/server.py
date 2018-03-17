1ccb1bbc145ff7b68d8aa2abebcc700c2abcbd67
dang3 <dang.dennis21@gmail.com>
2018-03-16 19:52:40 -0700

    Setup routes

--- Python Server/server.py
+++ Python Server/server.py
@@ -1,30 +1,39 @@
-import socket
-import os
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 
-def Main():
-	host = "127.0.0.1"
-	port = 5000
-	os.system("g++ main.cpp -o main")
-	
-	sock = socket.socket()
-	sock.bind((host,port))
-	sock.listen(1)
-	print "Server started..."
-	
-	con, addr = sock.accept()
-	print "Client connected ip<" + str(addr) + ">"
-	
-	while True:
-		messageIn = con.recv(1024)
-		if not messageIn:
-			break
-		if messageIn == 'y':
-			# run cpp file
-			os.system("./main")
-			con.send("cpp file executed")
-		else:
-			con.send("file not executed")
-	sock.close()
-	
+class Server(BaseHTTPRequestHandler):
+    def _set_headers(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+    def do_GET(self):
+        self._set_headers()
+        if self.path == '/':
+        	self.wfile.write("<html><body><h1>CSCVOP<br/>Hello World</h1></body></html>")
+        elif self.path == '/status':
+        	self.wfile.write("<html><body><h1>Give video(id) status</h1></body></html>")
+        elif self.path == '/video':
+        	self.wfile.write("<html><body><h1>Return video(id)</h1></body></html>")
+        
+    def do_POST(self):
+        self._set_headers()
+        if self.path == '/upload':
+        	self.wfile.write("<html><body><h1>Upload video</h1></body></html>")
+        
+def run(server_class=HTTPServer, handler_class=Server, port=5000):
+    server_address = ('localhost', port)
+    httpd = server_class(server_address, handler_class)
+    print "Server Started....."
+    try:
+    	httpd.serve_forever()
+    except KeyboardInterrupt:
+    	print '\nKeyboard Interrupt, shutting down server'
+    httpd.shutdown()
+
 if __name__ == "__main__":
-	Main()
+    from sys import argv
+
+    if len(argv) == 2:
+        run(port=int(argv[1]))
+    else:
+        run()
