0126022a7114f784178e4e75698078fc8295c5ad
deanljohnson <dean@deanljohnson.com>
2018-03-22 22:32:56 -0700

    Removed libjasper-dev from install.sh - causes errors on Ubuntu and is not needed

--- install.sh
+++ install.sh
@@ -29,7 +29,7 @@ sudo apt-get -y install clang-3.8 libc++-dev libc++abi-dev
 sudo apt-get -y install cmake
 sudo apt-get -y install libopenblas-dev liblapack-dev
 sudo apt-get -y install git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
-sudo apt-get -y install python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev checkinstall
+sudo apt-get -y install python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libdc1394-22-dev checkinstall
 echo "Essential dependencies installed."
 
 # OpenCV Dependency
